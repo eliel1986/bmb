9d42c76e103bc796468a9ed6cfd94a2cb0691f99
eliel1986 <eliel_1986@hotmail.com>
2022-11-28 21:33:21 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function mudaTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
 function clickMenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
